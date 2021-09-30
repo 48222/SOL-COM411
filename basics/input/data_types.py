017cb637ae4925e7490a6de504f159ec1ff528e0
Michael Mitrev <5mitrm28@solent.ac.uk>
2021-09-30 16:44:34 +0100

    Working on data types

---
+++ basics/input/data_types.py
@@ -0,0 +1,11 @@
+#calculate BMI
+print("What is your name human?")
+name = input()
+print("How old are you (in years)?")
+age = input()
+print("How tall are you (in meters)?")
+height = float(input())
+print("How much do you weight (in kilograms)?")
+weight = float(input())
+bmi = weight/height**2
+print(f"{name} you are {age} years old and your BMI is {bmi}")
