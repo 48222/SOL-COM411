55e115a20b67084c37dd8ba75e3f9f308b5bed59
Michael <you@domain.com>
2021-10-01 21:22:07 +0100

    Created a custom Python program, probably the worst you've seen:)

---
+++ basics/input/review.py
@@ -0,0 +1,33 @@
+# Custom program to show what I've learned
+print("Hello there! Welcome to my short Python program, let's start by telling me your name:")
+name = input()
+print(f"Right, Hi {name} I've got your name, now if you could tell me what music genre you like the most?")
+musgenre = input()
+print(f"Oh, how nice {musgenre} is my favorite music genre as well.")
+print(" \n\t\t\"Let me show you a trick, Tell me a number please\" ")
+number = int(input())
+print(f"{'♜' * number}")
+
+print("You've come to the end of my program, if you would like to see an ASCII art, please please enter.")
+input()
+print("""
+                            _.--~~~~~~~---------------._
+                          ,=-,.--#####----..--------.   `.
+                        ,'  ,/ _/_ ####   :!         `   ``.
+                       ,   ,/ [.[.]-=##   :!     /    `   `.`.
+                      ,   ,/   /_    )#   :!   // //   `    `.`.
+                     ,   /7    |_/   #    :!    // /    `     `.`.  ___,-
+                    .   ; |     \___/     :!             ;      `~`=    /
+              ___,--`--(  ;_____/__  \____:!__________,-'           `. /
+        __,--'         '~~         \  \ __`               ______      `.
+    _,-'                      _ ____\  \-o .             /ZZZZZ/        `.
+  ,'              turbo`     {_|_______)                                  `.
+ .------ ------ ---- ---, --  --- - --- -- .--  - - ---  -- -   -- --- - --!
+ !__'     ,-~~~-.                                     ,-~~~-.             /|
+ | ,     ' ,mMm. `      .                  .         ' ,mMm. `           /-!
+ ;-_____'.d-' `-b.`_____ __________________ ________'.d-' `-b.`_________/FS;_
+:      ;.d'\ _ /`b.\    .                  '       ;.d'\ _ /`b.\             :
+!====== d;-.' `.-:b ==== ================= ======== d;-.' `.-:b =============;
+`-.   : H  |   |  D :   `                 '       : H  |   |  D :     _ __,-'
+   `-.! C:-`._,'-;P ;_____________________________! C:-`._,'-;P ;__________7
+""")
