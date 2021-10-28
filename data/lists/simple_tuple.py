05f3af7474c84862965352cffc1b1d5215207d1f
Michael Mitrev <5mitrm28@solent.ac.uk>
2021-10-28 16:32:22 +0100

    Added code to demonstrate a simple tuple

---
+++ data/lists/simple_tuple.py
@@ -0,0 +1,12 @@
+def likelihood():
+    likelihoods = (50, 38, 27, 99, 4)
+    return min(likelihoods)
+
+
+def run():
+    liks = likelihood()
+    print(f"Minimum likelihood of falling: {likelihood()}%")
+
+
+if __name__ == "__main__":
+    run()
