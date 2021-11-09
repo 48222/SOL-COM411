6314b0bc16b2ea55682daa1c1bcbb7b4afc0af01
Michael <you@domain.com>
2021-11-09 19:00:38 +0000

    Added code to demonstrate the implementation of a Text User Interface

--- data/olympics/tui.py
+++ data/olympics/tui.py
@@ -14,19 +14,31 @@ def error(msg):
 
 
 def menu():
-    print("""
+    print(f"""
     Please select one of the options:
-    [years] List unique years
-    [tally] Tally up medals
-    [team] Tally up medals for each team
-    [exit[ Exit the menu
+    {"[years]":<10} List unique years
+    {"[tally]":<10} Tally up medals
+    {"[team]":<10} Tally up medals for each team
+    {"[exit]":<10} Exit the menu
     
     """)
-    print("Your selection:")
-    tally = int(input())
-    return tally
+    tally = input("Your selection:")
+    return tally.strip().lower()
 
 
 def display_medal_tally(tally):
-    tally = {"Gold": 10, "Silver": 5, "Bronze": 2}
-    print(f"")
+    print(f"| {'Gold'}:<10 | {tally['Gold']:<10} |")
+    print(f"| {'Silver'}:<10 | {tally['Silver']:<10} |")
+    print(f"| {'Bronze'}:<10 | {tally['Bronze']:<10} |")
+
+
+def display_team_medal_tally(team_tally):
+    for team, tally in team_tally.items():
+        print(team)
+        print(f"\tGold:{tally['Gold']}, Silver:{tally['Silver']}, Bronze:{tally['Bronze']}")
+
+
+def display_years(years):
+    sorted_years = sorted(years, reverse=True)
+    for year in sorted_years:
+        print(year)
